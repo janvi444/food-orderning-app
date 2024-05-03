2d51c783da338722c4ae951e4597b3699339b0d7
janvi444 <janvisethi44@gmail.com>
2024-05-03 10:12:23 +0530

    commit-j-1

---
+++ translucent-food-eCommerce-main/src/administrator/administrator.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Admin = () => {
+  return (
+    <a
+      className="w-full text-zinc-500 font-mono no-underline hover:text-zinc-400 flex items-center justify-center text-sm font-extralight cursor-default md:cursor-pointer"
+      href="https://www.github.com/janvi444"
+      target="_blank"
+    >
+      Designed & built by - chitkara
+    </a>
+  );
+};
+
+export default Admin;
